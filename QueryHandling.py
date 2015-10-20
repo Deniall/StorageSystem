b5a5ac821f1e28afe4c43ee6f236ffadf10c6faf
Niall Walsh <nialternate@gmail.com>
2015-10-20 12:00:52 +0100

    Update QueryHandling.py

--- QueryHandling.py
+++ QueryHandling.py
@@ -1,7 +1,9 @@
 def queryHandling():
+    location = [[(1,2),(2,1),(3,3)],[(1,9),(3,8),(2,4)]]
+    item = [(6,2),(9,3),(10,5)]
     i = 0
     k = 0
-    itemQuery = raw_input("Enter the item you wish to query")
+    itemQuery = int(raw_input("Enter the item number you wish to query: "))
     #location is the array of different locations
     #locations is the singular location
     #items is the different elements inside a singular location
@@ -10,13 +12,9 @@ def queryHandling():
     for locations in location:
         for items in locations:
             if items[0] == itemQuery:
-                print items[1]
-                print "Item"+i" is in location"+k
+                print "Item "+str(i)+" is in Location "+str(k)
+                print "There is "+str(items[1])+" of Item"+str(i)+" in Location"+str(k)
                 i+=1
-                return
-            else:
-                print "There is none of this item in location"+k
-                i+=1
-                return
+            
         k+=1;
     
